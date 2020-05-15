1819fb2b886076ded2839b7df7b6b9eeaedac59b
Maciej <maciej@contact.apurer.io>
2020-05-15 05:34:15 +0200

    testing 2

---
+++ swift-action-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-action-test
+//
+//  Created by Apurer on 15/05/2020.
+//  Copyright © 2020 Apurer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
